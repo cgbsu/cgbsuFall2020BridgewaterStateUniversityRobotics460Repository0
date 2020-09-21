dcbca15360cad86fec5612821aa6386d67d9615f
cgbsu@github.com <TheFloatingBrain@gmail.com>
2020-09-21 13:24:00 -0400

    Added new project

--- LightFinder/Main.go
+++ LightFinder/Main.go
@@ -14,16 +14,8 @@ func robotRunLoop(gopigo3 *g.Driver, lightSensor *aio.GroveLightSensorDriver) {
 		sensorVal, err := lightSensor.Read()
 		if err != nil {
 			fmt.Errorf("Error reading sensor %+v", err)
-		} else {
-			if sensorVal >= 1000 {
-				gopigo3.SetMotorDps( g.MOTOR_LEFT, 180 )
-				gopigo3.SetMotorDps( g.MOTOR_RIGHT, -180 )
-			} else {
-				gopigo3.SetMotorDps( g.MOTOR_LEFT, 0 )
-				gopigo3.SetMotorDps( g.MOTOR_RIGHT, 0 )
-			}
 		}
-		time.Sleep(time.Second)
+		fmt.print( sensorVal )
 	}
 }
 
