485e77795e6c6103b1167a64fa8129bba2d608c8
cgbsu@github.com <TheFloatingBrain@gmail.com>
2020-10-07 13:22:26 -0400

    Incrament

--- LidarDemo/main.go
+++ LidarDemo/main.go
@@ -38,7 +38,7 @@ func RobotMainLoop(piProcessor *raspi.Adaptor, gopigo3 *g.Driver, lidarSensor *i
 		fmt.Println(lidarReading)
 		fmt.Println(message)
 		time.Sleep(time.Second * 3)*/
-		if lidarReading < OneFootInCentimetersConstant {
+		if float( lidarReading ) < OneFootInCentimetersConstant {
 
 		} else {
 
