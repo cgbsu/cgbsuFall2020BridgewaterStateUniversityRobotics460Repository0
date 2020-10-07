b14c466492c66a6e0b66d610acb7bec880827338
cgbsu@github.com <TheFloatingBrain@gmail.com>
2020-10-07 13:16:58 -0400

    Incrament

--- LidarDemo/main.go
+++ LidarDemo/main.go
@@ -10,57 +10,51 @@ import (
 	"time"
 )
 
-func robotMainLoop(piProcessor *raspi.Adaptor, gopigo3 *g.Driver, lidarSensor *i2c.LIDARLiteDriver,
+/*
+	* : Robot will start within a foot of the box, approximatley perpendicular.
+	* : Robot will try to maintain at least an inch's distance from the box
+	* : First goal, measure a side of the box.
+	* : Second goal, measure full box.
+*/
 
-/*lcd *i2c.GroveLcdDriver*/) {
-	//err := lcd.Start()
-	//if err != nil {
-	//	fmt.Println("error starting lcd")
-	//}
+const OneFootInCentimetersConstant := 29.4
+
+func Uniform
+
+func RobotMainLoop(piProcessor *raspi.Adaptor, gopigo3 *g.Driver, lidarSensor *i2c.LIDARLiteDriver,
+
 	err := lidarSensor.Start()
 	if err != nil {
 		fmt.Println("error starting lidarSensor")
 	}
-	//err = lcd.SetRGB(100, 0, 0)
-	//if err != nil {
-	//	fmt.Println("Error setting lcd color")
-	//}
-	for { //loop forever
+	gopigo3.Every( time.Millisecond, func() { //loop forever
 		lidarReading, err := lidarSensor.Distance()
 		if err != nil {
 			fmt.Println("Error reading lidar sensor %+v", err)
 		}
-		message := fmt.Sprintf("Lidar Reading: %d", lidarReading)
-		//err = lcd.Clear()
-		//if err != nil {
-		//	fmt.Println("error clearing lcd")
-		//}
-		//err = lcd.Home()
-		//if err != nil {
-		//	fmt.Println("error homeing lcd")
-		//}
-		//err = lcd.Write(message)
-		//if err != nil {
-		//	fmt.Println("error writing to LCD")
-		//}
+		/*message := fmt.Sprintf("Lidar Reading: %d", lidarReading)
 		fmt.Println(lidarReading)
 		fmt.Println(message)
-		time.Sleep(time.Second * 3)
-	}
+		time.Sleep(time.Second * 3)*/
+		if lidarReading < OneFootInCentimetersConstant {
+
+		} else {
+
+		}
+	} )
 }
 
 func main() {
 	raspberryPi := raspi.NewAdaptor()
 	gopigo3 := g.NewDriver(raspberryPi)
 	lidarSensor := i2c.NewLIDARLiteDriver(raspberryPi)
-	//lcd := i2c.NewGroveLcdDriver(raspberryPi)
 	lightSensor := aio.NewGroveLightSensorDriver(gopigo3, "AD_2_1")
 	workerThread := func() {
-		robotMainLoop(raspberryPi, gopigo3, lidarSensor /*lcd*/)
+		RobotMainLoop(raspberryPi, gopigo3, lidarSensor)
 	}
 	robot := gobot.NewRobot("Gopigo Pi4 Bot",
 		[]gobot.Connection{raspberryPi},
-		[]gobot.Device{gopigo3, lidarSensor /*lcd,*/, lightSensor},
+		[]gobot.Device{gopigo3, lidarSensor, lightSensor},
 		workerThread)
 
 	robot.Start()
