966a7cc38c437c6589b98cbcf9d4b250ea903623
cgbsu@github.com <TheFloatingBrain@gmail.com>
2020-10-23 12:58:53 -0400

    Incrament

---
+++ StopBot/main.go
@@ -0,0 +1,24 @@
+import (
+	"fmt"
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/aio"
+	"gobot.io/x/gobot/drivers/i2c"
+	g "gobot.io/x/gobot/platforms/dexter/gopigo3"
+	"gobot.io/x/gobot/platforms/raspi"
+	// "math"
+)
+
+func main() {
+	raspberryPi := raspi.NewAdaptor()
+	gopigo3 := g.NewDriver(raspberryPi)
+	workerThread := func() {
+		gopigo3.SetMotorDps( g.MOTOR_LEFT, 0 )
+		gopigo3.SetMotorDps( g.MOTOR_RIGHT, 0 )	
+	}
+	robot := gobot.NewRobot("Gopigo Pi4 Bot",
+		[]gobot.Connection{raspberryPi},
+		[]gobot.Device{gopigo3, lidarSensor, lightSensor},
+		workerThread)
+	robot.Start()
+	robot.Stop()
+}
