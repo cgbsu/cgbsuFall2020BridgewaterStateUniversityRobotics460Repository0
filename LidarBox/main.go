ca3882df87d679cf0cd89fca9cdb7c68dbbed93a
cgbsu@github.com <TheFloatingBrain@gmail.com>
2020-10-23 15:44:09 -0400

    Incrament

--- LidarBox/main.go
+++ LidarBox/main.go
@@ -92,7 +92,7 @@ func ( self *Average ) AtDesiredSampleCount() bool {
 const MaxInitializationSamplesConstant = 10
 const OutOfBoundsDistanceConstant = 50
 const MaxOutOfBoundSamplesConstant = 5
-const CornerTurnAngleConstant = 90.0
+const CornerTurnAngleConstant = math.Pi / 2.0//90.0
 
 type Side struct { 
 	foundBox, goalDistanceFound, measuredSide bool
