7d4242a3ac135f9140a1405c10a9c23738c5ce92
cgbsu@github.com <TheFloatingBrain@gmail.com>
2020-10-26 15:02:34 -0400

    Incrament

--- LidarBox/main.go
+++ LidarBox/main.go
@@ -371,7 +371,7 @@ func ( self *Side ) Reset( robot *Robot ) bool {
 	return false
 }
 
-const ErrorConstant = 1.0 / ( 2.43 )
+const ErrorConstant = 1.0 // 1.0 / ( 2.43 )
 
 
 func RobotMainLoop(piProcessor *raspi.Adaptor, gopigo3 *g.Driver, lidarSensor *i2c.LIDARLiteDriver ) {
