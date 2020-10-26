25d87d7743fb8856333824f712e9efdb9b7505aa
cgbsu@github.com <TheFloatingBrain@gmail.com>
2020-10-26 14:41:48 -0400

    Incrament

--- LidarBox/main.go
+++ LidarBox/main.go
@@ -350,9 +350,9 @@ func ( self *Side ) MeasureSide( robot *Robot, loopRuntimeInSeconds float64 ) bo
 
 func ( self *Side ) Reset( robot *Robot ) bool {
 	if self.needsToTurn == true {
-		if self.turnResetCount < MaxTurnRestConstant {
+		if robot.lidarReading < OutOfBoundsDistanceConstant {//self.turnResetCount < MaxTurnRestConstant {
 			robot.Move( -100, -50 )
-			self.turnResetCount += 1
+			// self.turnResetCount += 1
 		} else {
 			self.needsToTurn = false
 		}
