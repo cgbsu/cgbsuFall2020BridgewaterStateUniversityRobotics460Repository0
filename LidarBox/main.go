d98d78aa5eadd87ace11966939b415c40162f6b6
cgbsu@github.com <TheFloatingBrain@gmail.com>
2020-10-28 14:37:32 -0400

    Incrament

--- LidarBox/main.go
+++ LidarBox/main.go
@@ -363,7 +363,7 @@ func ( self *Side ) MeasureSide( robot *Robot, loopRuntimeInSeconds float64 ) bo
 			self.outOfBoundsDistance.AddSample( robot.lidarReading )
 		} else {
 			self.outOfBoundsDistance.Clear()
-			previousDirection := robot.currentDirection
+			//previousDirection := robot.currentDirection
 			if self.Creep( robot, loopRuntimeInSeconds ) == true {
 				fmt.Println( "Change direction" )
 				// self.AddToTotalDistance( robot, previousDirection )
